112e8d65b8b394e18f3ae29b9a7362b02a3334fa
egalland <emmanuel.galland117@gmail.com>
2018-06-22 08:48:57 +0200

    maj commentaires

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// FONCTION LORSQU'ON SORT DU CHAMPS DE TEXTE
 function focusOut(){
+  // DECLARATION ET RECUPERATION DES VARIABLES
   var lastname = document.getElementById('lastname').value;
+  // BOITE DE DIALOGUE REMERCIANT DE LA PARTICIPATION
   alert('Merci de votre participation');
 }
